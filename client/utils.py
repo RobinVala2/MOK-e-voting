291f2214b652d55e094b7c2602a1915d7bba763c
RobinVala2 <241124@vutbr.cz>
2025-12-27 19:48:38 +0100

    remove server files

--- client/utils.py
+++
@@ -1,8 +0,0 @@
-import httpx, asyncio
-
-SERVER = "http://127.0.0.1:8000"
-
-async def run_hyperion():
-    async with httpx.AsyncClient() as client:
-        r = await client.post(f"{SERVER}/hyperion")
-        print("[HYPERION]", r.json())
